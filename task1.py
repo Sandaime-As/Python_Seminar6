3f53aa32db76d9115132d6556d635af070bc8bbd
Sandaime-As <asenov1998@mail.ru>
2022-12-27 20:56:41 +0600

    add task2

--- task1.py
+++ task1.py
@@ -5,5 +5,12 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
+# num=input('Введите число: ')
+# sum=0
+# for i in num:
+#     if i != '.':
+#         sum+=int(i)
+# print(f'Сумма {num} равна: {sum}')
+
 number = float(input('Введите число: '))
 print(sum(map(int, [x for x in str(number) if x.isnumeric()])))
